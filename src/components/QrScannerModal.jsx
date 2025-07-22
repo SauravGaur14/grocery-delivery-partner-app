a867d6ecdc5c19780c1ee7e78eed207b4ba020f6
Saurav Gaur <gaur.saurav14@gmail.com>
2025-07-22 12:58:23 +0530

    QR Scanner UI

--- src/components/QrScannerModal.jsx
+++ src/components/QrScannerModal.jsx
@@ -44,13 +44,38 @@ export default function QrScannerModal({ visible, onClose, onScanned }) {
             No access to camera
           </Text>
         ) : (
-          <CameraView
-            style={{ flex: 1 }}
-            barcodeScannerSettings={{
-              barcodeTypes: ["qr"], // Only scan QR codes
-            }}
-            onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
-          />
+          <View className="flex-1">
+            <CameraView
+              style={{ flex: 1 }}
+              barcodeScannerSettings={{
+                barcodeTypes: ["qr"],
+              }}
+              onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+            />
+
+            {/* Overlay Frame + Text */}
+            <View className="absolute top-0 left-0 right-0 bottom-0 justify-center items-center">
+              <Text className="text-white mb-10 text-2xl">Scan QR Code</Text>
+              {/* Corner Frame Container */}
+              <View className="w-64 h-64 relative">
+                {/* Top Left */}
+                <View className="absolute top-0 left-0 w-6 h-1 bg-white" />
+                <View className="absolute top-0 left-0 w-1 h-6 bg-white" />
+
+                {/* Top Right */}
+                <View className="absolute top-0 right-0 w-6 h-1 bg-white" />
+                <View className="absolute top-0 right-0 w-1 h-6 bg-white" />
+
+                {/* Bottom Left */}
+                <View className="absolute bottom-0 left-0 w-6 h-1 bg-white" />
+                <View className="absolute bottom-0 left-0 w-1 h-6 bg-white" />
+
+                {/* Bottom Right */}
+                <View className="absolute bottom-0 right-0 w-6 h-1 bg-white" />
+                <View className="absolute bottom-0 right-0 w-1 h-6 bg-white" />
+              </View>
+            </View>
+          </View>
         )}
 
         <TouchableOpacity
